08529b020085728275aabe0aa422cb475f1a7766
Kohei Hasegawa <ameutau@gmail.com>
2015-10-30 09:05:41 +0900

    Use Hyper

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+extern crate hyper;
+
+use std::io::Read;
+
+use hyper::Client;
+use hyper::header::Connection;
+
 fn main() {
-    println!("Hello, world!");
+    let client = Client::new();
+
+    let mut res = client.get("http://localhost:1235/?new")
+        .header(Connection::close())
+        .send().unwrap();
+
+    let mut body = String::new();
+    res.read_to_string(&mut body).unwrap();
+
+    println!("Response: {}", body);
 }
