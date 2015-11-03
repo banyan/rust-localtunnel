a422cf858d312cf6b0f25029b34a225ba04cb000
Kohei Hasegawa <ameutau@gmail.com>
2015-11-03 12:49:37 +0900

    try to pipe

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,38 @@
 extern crate hyper;
 
-use std::io::Read;
-
 use hyper::Client;
 use hyper::header::Connection;
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::io::{Read, Write};
+
+fn handle_client(mut conn_remote: TcpStream) -> () {
+    let host = "localhost:3000";
+    let mut conn_local = TcpStream::connect(host).unwrap();
+
+    let mut buf = [0; 512];
+    loop {
+        let _ = match conn_remote.read(&mut buf) {
+            Err(e) => panic!("Got an error: {}", e),
+            Ok(m) => {
+                if m == 0 {
+                    break;
+                }
+                m
+            },
+        };
+
+        match conn_local.write(&buf) {
+            Err(_) => break,
+            Ok(_) => continue,
+        }
+    }
+}
 
 fn main() {
     let client = Client::new();
 
-    let mut res = client.get("http://localtunnel:1235/?new")
+    let mut res = client.get("http://localhost:1236/?new")
         .header(Connection::close())
         .send().unwrap();
 
@@ -16,4 +40,10 @@ fn main() {
     res.read_to_string(&mut body).unwrap();
 
     println!("Response: {}", body);
+
+    let listener = TcpListener::bind("localhost:1236").unwrap();
+    for stream in listener.incoming() {
+        let s = stream.unwrap();
+        handle_client(s);
+    }
 }
